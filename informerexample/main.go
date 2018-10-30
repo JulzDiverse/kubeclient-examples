c9af9f23ea7f3e6d87fa1cea3b4b9436c013fb36
Julian Skupnjak <julian.skupnjak@gmail.com>
2018-10-30 16:33:53 +0100

    changes for informer-side-car example

--- informerexample/main.go
+++ informerexample/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
+	"time"
 
 	v11 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -18,7 +19,8 @@ import (
   It comes with a simple Factory for all kubernetes resources.
 */
 func main() {
-	kubeconfig := os.Getenv("KUBECONFIG")
+	//kubeconfig := os.Getenv("KUBECONFIG")
+	kubeconfig := ""
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
 		panic(err.Error())
@@ -29,28 +31,35 @@ func main() {
 		panic(err.Error())
 	}
 
-	factory := informers.NewSharedInformerFactoryWithOptions(clientset, 5000, informers.WithNamespace("eirini"))
+	fmt.Println("POD_NAME:", os.Getenv("POD_NAME"))
+	factory := informers.NewSharedInformerFactoryWithOptions(clientset, 20*time.Second, informers.WithNamespace("opi"), informers.WithTweakListOptions(listTweaker))
 	informer := factory.Core().V1().Pods().Informer()
 	stopper := make(chan struct{})
 	defer close(stopper)
 	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			mObj := obj.(v1.Object)
+		//AddFunc: func(obj interface{}) {
+		//mObj := obj.(v1.Object)
 
-			name := mObj.GetName()
-			log.Printf("\nNew Pod Added to Store: %s", name)
-		},
+		//name := mObj.GetName()
+		//log.Printf("\nNew Pod Added to Store: %s", name)
+		//},
 		UpdateFunc: func(obj interface{}, newObj interface{}) {
+			fmt.Println("UPDATE")
 			mObj := obj.(*v11.Pod)
-			log.Printf("Pod IP:%s", mObj.Status.PodIP)
+			fmt.Printf("Pod IP updated:%s", mObj.Status.PodIP)
 		},
 		DeleteFunc: func(obj interface{}) {
 			mObj := obj.(v1.Object)
 
-			log.Printf("\nPod Removed to Store: %s", mObj.GetName())
-			log.Printf("Labels: %s\n", mObj.GetLabels())
+			fmt.Printf("\nPod Removed from Store: %s", mObj.GetName())
+			fmt.Printf("Labels: %s\n", mObj.GetLabels())
 		},
 	})
 
 	informer.Run(stopper)
+}
+
+func listTweaker(listOptions *v1.ListOptions) {
+	pod := os.Getenv("POD_NAME")
+	listOptions.LabelSelector = fmt.Sprintf("statefulset.kubernetes.io/pod-name=%s", pod)
 }
